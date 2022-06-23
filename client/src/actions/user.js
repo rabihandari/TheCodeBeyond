0f9789601314fa2e371163125917bdb898a99b35
Rabih Andary <rabih@cigotracker.com>
2022-06-23 20:48:39 +0300

    Fixed getUser token errors

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -3,15 +3,16 @@ import * as api from '../api';
 
 export const getSettings = () => async (dispatch) => {
     try {
-      const resposnse = await api.getUserSettings();
+      const response = await api.getUserSettings();
 
-      if (Object.keys(resposnse?.data.account).includes('confirmed')){
-        if(!resposnse?.data.account.confirmed){
+      if (Object.keys(response?.data).includes("account")){
+        console.log(response?.data.account.confirmed);
+        if(!response?.data.account.confirmed){
           dispatch({ type: actionTypes.SHOW_ALERT, payload: { message: 'Your account is not activated yet!', severity: 'warning' } })
         }
       } 
       
-      dispatch({ type: actionTypes.SETTINGS, payload: resposnse?.data });
+      dispatch({ type: actionTypes.SETTINGS, payload: response?.data });
     } catch (error) {
         return Promise.reject(error);
     }
--- server/middlewares/getUser.js
+++ server/middlewares/getUser.js
@@ -15,14 +15,56 @@ const getUser = async (req, res, next) => {
   try {
 
     // Decode token...
-    let tokenData = jwt.decode(token);
+    let tokenData = jwt.verify(token, SECRET_OR_KEY);
     req.userId = tokenData.id;   
-    
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+
+    try {
+      // Check for refresh token
+     jwt.verify(refreshToken, SECRET_OR_KEY2);
+
+     // Refresh the tokens
+     let decodedData = jwt.decode(token);
+     const [newToken, newRefreshToken] = await createTokens(token, refreshToken, decodedData.id);
+ 
+     // Set headers
+     res.cookie('token', newToken, { httpOnly: true, secure: true, expires: new Date(2147483647000), sameSite: 'none' });
+     res.cookie('refreshToken', newRefreshToken, { httpOnly: true,  expires: new Date(2147483647000), sameSite: 'none' });
+     res.cookie('authType', authType, { expires: new Date(2147483647000), secure: true, sameSite: 'none' });
+     req.userId = decodedData.id;
+
+    } catch (error){
+      console.log(error.message);
+    }
+
   }
   
   next();
+  
 };
 
 export default getUser;
+
+
+export const createTokens = async (token, refreshToken, id) => {
+
+
+  const createToken = jwt.sign(
+    { id: id },
+    SECRET_OR_KEY,
+    {
+      expiresIn: '1m',
+    },
+  );
+
+  const createRefreshToken = jwt.sign(
+    {},
+    SECRET_OR_KEY2,
+    {
+      expiresIn: '1y',
+    },
+  );
+
+  return Promise.all([createToken, createRefreshToken]);
+};
